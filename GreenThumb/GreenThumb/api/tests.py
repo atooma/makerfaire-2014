c625cf81f02f887d6c4243379b12e94104d1d1d3
Andrea Stagi <stagi.andrea@gmail.com>
2014-09-25 01:42:38 +0200

    Added tests for single sensor API

--- GreenThumb/GreenThumb/api/tests.py
+++ GreenThumb/GreenThumb/api/tests.py
@@ -7,10 +7,17 @@ import json
 
 class SensorTest(TestCase):
 
-    def test_get_correct_values(self):
+    def test_get_all_sensor_correct_values(self):
         DHTSensor.objects.create(id=12, temperature=24, humidity=32)
         response = self.client.get('/api/sensors/dht/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['count'], 1)
         self.assertEqual(response.data['results'][0]['temperature'], 24)
         self.assertEqual(response.data['results'][0]['humidity'], 32)
+
+    def test_get_single_sensor_correct_values(self):
+        DHTSensor.objects.create(id=12, temperature=24, humidity=32)
+        response = self.client.get('/api/sensors/dht/{}/'.format(12))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['temperature'], 24)
+        self.assertEqual(response.data['humidity'], 32)
