7eceebca334c17a5c6db6bb6b79dbfa28a409a08
Andrea Stagi <stagi.andrea@gmail.com>
2014-09-29 17:22:54 +0200

    First Nanpy integration

--- ChickenfootREST/chickenfoot.py
+++ ChickenfootREST/chickenfoot.py
@@ -1,26 +1,68 @@
 from flask import Flask, request, jsonify
 from config import config
+from nanpy import serial_manager, Arduino, Tone
 import os
+import time
 
 app = Flask(__name__)
 config_name = (os.getenv('FLASK_CONFIG') or 'default')
 app.config.from_object(config[config_name])
 
+motor1_cp1 = 4
+motor1_cp2 = 5
+motor1_ep = 10
+motor2_cp1 = 2
+motor2_cp2 = 3
+motor2_ep = 9
+motor_speed = 1000
+lights_pin = 7
+beep_pin = 8
+
 @app.route('/move', methods=['POST'])
 def move():
     movetype = request.json['type']
     duration = request.json['duration']
+
+    if movetype == 'left':
+        Arduino.digitalWrite(motor1_cp1, 0)
+        Arduino.digitalWrite(motor1_cp2, 1)
+        Arduino.analogWrite(motor1_ep, motor_speed)
+    elif movetype == 'right':
+        Arduino.digitalWrite(motor1_cp1, 1)
+        Arduino.digitalWrite(motor1_cp2, 0)
+        Arduino.analogWrite(motor1_ep, motor_speed)
+    elif movetype == 'up':
+        Arduino.digitalWrite(motor2_cp1, 0)
+        Arduino.digitalWrite(motor2_cp2, 1)
+        Arduino.analogWrite(motor2_ep, motor_speed)
+    elif movetype == 'down':
+        Arduino.digitalWrite(motor2_cp1, 1)
+        Arduino.digitalWrite(motor2_cp2, 0)
+        Arduino.analogWrite(motor2_ep, motor_speed)
+    else:
+        return jsonify({ 'error' : 'use type up, down, left or right' }), 400
+    time.sleep(duration/1000)
+
     return jsonify({ 'status' : 'success' }), 200
 
 @app.route('/lights', methods=['POST'])
 def lights():
     status = request.json['status']
+    if status == 'on':
+        Arduino.digitalWrite(lights_pin, 1)
+    elif status == 'off':
+        Arduino.digitalWrite(lights_pin, 0)
+    else:
+        return jsonify({ 'error' : 'use status on or off' }), 400
     return jsonify({ 'status' : 'success' }), 200
 
 @app.route('/beep', methods=['POST'])
 def beep():
     duration = request.json['duration']
+    tone = Tone(beep_pin)
+    #tone.play(Tone.NOTE_E2, duration/1000)
     return jsonify({ 'status' : 'success' }), 200
 
 if __name__ == "__main__":
+    serial_manager.open('/dev/tt.usbmodem1411')
     app.run()
--- ChickenfootREST/test.py
+++ ChickenfootREST/test.py
@@ -1,7 +1,9 @@
+import chickenfoot
 from chickenfoot import app
 from flask import url_for
 import unittest
 import json
+from mock import patch
 
 class TestCar(unittest.TestCase):
 
@@ -11,19 +13,42 @@ class TestCar(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_move_ok(self):
+    @patch('chickenfoot.Arduino.analogWrite')
+    @patch('chickenfoot.Arduino.digitalWrite')
+    def test_move_ok(self, mock_analogWrite, mock_digitalWrite):
+        with app.app_context():
+            move_types = ['up', 'down', 'left', 'right']
+            move_json = {
+                'duration' : 1,
+                'type' : 'up'
+            }
+            for move_type in move_types:
+                move_json['type'] = move_type
+                response = self.client.post(url_for('move'),
+                        data=json.dumps(move_json),
+                        content_type='application/json')
+
+                self.assertEqual(response.status_code, 200)
+
+    @patch('chickenfoot.Arduino.analogWrite')
+    @patch('chickenfoot.Arduino.digitalWrite')
+    def test_move_wrong_par(self, mock_analogWrite, mock_digitalWrite):
         with app.app_context():
             move_json = {
-                'duration' : 1000,
-                'type' : 'up'
+                'duration' : 1,
+                'type' : 'burp'
             }
             response = self.client.post(url_for('move'),
                     data=json.dumps(move_json),
                     content_type='application/json')
 
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 400)
 
-    def test_lights_ok(self):
+            self.assertEqual(mock_digitalWrite.called, False)
+            self.assertEqual(mock_analogWrite.called, False)
+
+    @patch('chickenfoot.Arduino.digitalWrite')
+    def test_lights_on_ok(self, mock_digitalWrite):
         with app.app_context():
             move_json = {
                 'status' : 'on'
@@ -33,14 +58,43 @@ class TestCar(unittest.TestCase):
                     content_type='application/json')
 
             self.assertEqual(response.status_code, 200)
+            mock_digitalWrite.assert_called_once_with(chickenfoot.lights_pin, 1)
 
-    def test_beep_ok(self):
+    @patch('chickenfoot.Arduino.digitalWrite')
+    def test_lights_off_ok(self, mock_digitalWrite):
         with app.app_context():
             move_json = {
-                'duration' : 1000,
+                'status' : 'off'
+            }
+            response = self.client.post(url_for('lights'),
+                    data=json.dumps(move_json),
+                    content_type='application/json')
+
+            self.assertEqual(response.status_code, 200)
+            mock_digitalWrite.assert_called_once_with(chickenfoot.lights_pin, 0)
+
+    @patch('chickenfoot.Arduino.digitalWrite')
+    def test_lights_wrong_par(self, mock_digitalWrite):
+        with app.app_context():
+            move_json = {
+                'status' : 'org'
+            }
+            response = self.client.post(url_for('lights'),
+                    data=json.dumps(move_json),
+                    content_type='application/json')
+
+            self.assertEqual(response.status_code, 400)
+            self.assertEqual(mock_digitalWrite.called, False)
+
+    @patch('chickenfoot.Tone')
+    def test_beep_ok(self, tone_mock):
+        with app.app_context():
+            move_json = {
+                'duration' : 1,
             }
             response = self.client.post(url_for('beep'),
                     data=json.dumps(move_json),
                     content_type='application/json')
 
             self.assertEqual(response.status_code, 200)
+            tone_mock.assert_called_once()
